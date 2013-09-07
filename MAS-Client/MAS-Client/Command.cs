ac7c7c8401626d12bf89fadf10ed6b6b5c0c92d0
SStalker <raphael@bluepanel.org>
2013-09-07 23:42:44 +0200

    First commit^^
    What can it do?
    * Connect to the server and receive commands
    * getActiveWindowTitle
    * getOSPlatform
    * SetWallpaper
    * GetWallpaper
    * startProgram
    * getOpenPrograms

---
+++ MAS-Client/MAS-Client/Command.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MAS_Client
+{
+    class Command
+    {
+        const int MAX_PATH = 260;
+        const int SPI_GETDESKWALLPAPER = 0x73;
+        const int SPI_SETDESKWALLPAPER = 0x14;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        public static void ausgabe(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static string getOSPlatform()
+        {
+            OperatingSystem os = Environment.OSVersion;
+
+            PlatformID platform = os.Platform;
+
+            return platform.ToString();
+        }
+
+        public static void SetWallpaper(string pathToFile)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, pathToFile, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        public static string GetWallpaper()
+        {
+            string wallpaper = new string('\0', MAX_PATH);
+            SystemParametersInfo(SPI_GETDESKWALLPAPER, (int)wallpaper.Length, wallpaper, 0);
+            return wallpaper.Substring(0, wallpaper.IndexOf('\0'));
+        }
+
+
+        public static void startProgram(string exe, Array args)
+        {
+            Process prog = new Process();
+
+            prog.StartInfo.FileName = exe;
+            prog.StartInfo.Arguments = "";
+
+            prog.Start();
+        }
+
+        public static void getOpenPrograms()
+        {
+            foreach (Process p in Process.GetProcesses("."))
+            {
+                try
+                {
+                    if (p.MainWindowTitle.Length > 0)
+                    {
+                        Console.WriteLine("\r\n");
+                        Console.WriteLine("\r\n Window Title:" + p.MainWindowTitle.ToString());
+                        Console.WriteLine("\r\n Process Name:" + p.ProcessName.ToString());
+                        Console.WriteLine("\r\n Window Handle:" + p.MainWindowHandle.ToString());
+                        Console.WriteLine("\r\n Memory Allocation:" + p.PrivateMemorySize64.ToString());
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ MAS-Client/MAS-Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+
+namespace MAS_Client
+{
+    class Program
+    {
+        static NetworkStream stream;
+        static Int32 port = 13000;
+        static TcpClient client;
+        static String serverIP = "192.168.2.101";
+        static Byte[] data;
+
+
+        static void Main(string[] args)
+        {
+            //SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+
+            Console.WriteLine(Command.getOSPlatform());
+            Command.SetWallpaper(@"C:\Users\SStalker\Pictures\wallbase cc\wallpaper-1353834.jpg");
+
+            Command.startProgram("notepad.exe",null);
+
+            Console.WriteLine(Command.GetWallpaper());
+
+            Console.WriteLine(Command.GetActiveWindowTitle());
+
+            Command.getOpenPrograms();
+            Connect(serverIP);
+            
+            if (client != null)
+            {
+                while(client.Connected)
+                {                    
+                    if (stream.DataAvailable) 
+                    {
+                        // Buffer to store the response bytes.
+                        data = new Byte[256];
+
+                        // String to store the response ASCII representation.
+                        String responseData = String.Empty;
+
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine("Received: {0}", responseData);
+                    }                    
+                }
+            }
+        }
+
+        static void Connect(String server)
+        {
+            try
+            {
+                // Create a TcpClient.
+                client = new TcpClient(server, port);
+
+                // Get a client stream for reading and writing
+                stream = client.GetStream();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
+
+
